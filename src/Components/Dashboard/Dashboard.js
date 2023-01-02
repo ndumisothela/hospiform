c15d08d2b365e44b1a8059e693e885e068d99e2d
Ndumiso Thela <ndumisothela480@gmail.com>
2023-01-02 22:36:23 +0200

    googleform embed

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Dashboard.css";
 
-const formUrl = "https://docs.google.com/forms/u/0/";
-
 function Dashboard() {
   return (
     <div className="dash_container">
-      <div className="left_side"></div>
+      <div className="left_side">
+        {" "}
+        <iframe
+          className="google_form"
+          id="googleForm"
+          src="https://docs.google.com/forms/d/e/1FAIpQLScaITaTN45SuI7lizHDnKJKgYapf_JMGEZHWSLmbSKVTj3lrA/viewform?embedded=true"
+          width="700"
+          height="520"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
       <div className="right_side">
         <p>
           Medical Care appointmed™ is a convenient medical booking service
           designed to assist YOU with all your medical appointments. Get access
           to our extensive Medical Care network of healthcare professionals at
-          the hospitals. <br />
+          the hospital. <br />
           <br />
           No membership, no fees, just hassle-free healthcare to suit your
           unique needs.
@@ -21,9 +33,9 @@ function Dashboard() {
         <a
           type="submit"
           className="form_btn"
-          target="_top"
+          target="_blank"
           rel="noreferrer"
-          href={`${formUrl}`}
+          href={"#googleForm"}
         >
           Create Form
         </a>
